14c0bb01b1fdc2c94d6efea47f6e773304963182
John Daugherty <johndaugherty1986@gmail.com>
2017-08-03 01:50:57 -0500

    finished deal, shuffle, contains, and creeat_deck functions. taking a break for the night

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,2 +1,26 @@
 defmodule Cards do
+
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Clubs", "Spades", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+
+
 end
